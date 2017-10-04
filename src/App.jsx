616260b96f25a06dc7aefa67d31c4c309245d26b
Isaac Vallee <isaacvallee@gmail.com>
2017-10-04 20:17:27 +0000

    code cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,20 @@ import React, { Component } from 'react';
 import MessageList from './MessageList.jsx';
 import ChatBar from './ChatBar.jsx';
 
-
 class App extends Component {
 
   constructor(props) {
     super(props);
-    
+
     this.state = {
-      currentUser: {name: 'Bob'},
-      messages: [
-        // {
-        //   id: 1,
-        //   username: 'Bob',
-        //   content: 'Has anyone seen my marbles?'
-        // },
-        // {
-        //   id: 2,
-        //   username: 'Anonymous',
-        //   content: 'No, I think you lost them. You lost your marbles Bob. You lost them for good.'
-        // }
-      ]
-    };   
+      currentUser: '',
+      messages: []
+    };
     this.addMessage = this.addMessage.bind(this);
   }
 
   componentDidMount() {
     console.log('componentDidMount <App />');
-
 
     this.socket = new WebSocket(`ws://${location.hostname}:3001`);
 
@@ -37,26 +24,23 @@ class App extends Component {
     }
 
     this.socket.onmessage = (event) => {
-      const inMessage = JSON.parse(event.data);
+      const incoming = JSON.parse(event.data);
       const tempMessages = this.state.messages;
-      tempMessages.push(inMessage);
+      tempMessages.push(incoming);
       this.setState(tempMessages);
-      console.log(JSON.parse(event.data));
     }
-
   }
-  
+
   addMessage(text, user) {
     console.log('Posting new message...');
-    
-    this.setState({currentUser: {name: user}});
-    this.socket.send(JSON.stringify({username: user, content: text}))
-    
+
+    this.setState({ currentUser: { name: user } });
+    this.socket.send(JSON.stringify({ username: user, content: text }))
+
   }
 
-
   render() {
-    console.log("Rendering <App/>");
+    console.log('Rendering <App/>');
     return (
       <div>
         <nav className="navbar">
@@ -64,7 +48,7 @@ class App extends Component {
         </nav>
         <MessageList messages={this.state.messages} />
 
-        <ChatBar currentUser={this.state.currentUser.name} addMsg={this.addMessage}/>
+        <ChatBar currentUser={this.state.currentUser.name} addMsg={this.addMessage} />
       </div>
     );
   }
