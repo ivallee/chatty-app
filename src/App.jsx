c5394644087eb8478bb2f211004e51bcbab1387f
Isaac Vallee <isaacvallee@gmail.com>
2017-10-05 00:51:42 +0000

    change user sends message to server

--- src/App.jsx
+++ src/App.jsx
@@ -26,26 +26,29 @@ class App extends Component {
 
     this.socket.onmessage = (event) => {
       const incoming = JSON.parse(event.data);
+      console.log(incoming.type);
       const tempMessages = this.state.messages;
       tempMessages.push(incoming);
       this.setState(tempMessages);
     }
   }
 
-  changeUser(user) {
-    console.log('Changing user to ' + user);
-    this.setState({ currentUser: { name: user } });
-
+  changeUser(newUser) {
+    const oldUser = this.state.currentUser.name;
+    if (newUser !== oldUser) {
+      this.setState({ currentUser: { name: newUser } });
+      this.socket.send(JSON.stringify({content: `${oldUser} has changed their name to ${newUser}`, type: 'postNotification' }))
+    }
   }
-
 
   addMessage(text, user) {
     console.log('Posting new message...');
     
-    if (user !== this.state.currentUser.name) {
-      this.changeUser(user);
-    }
-    this.socket.send(JSON.stringify( { username: (user ? user : 'Anonymous'), content: text } ))
+    this.changeUser(user);
+
+    this.socket.send(JSON.stringify( { username: (user ? user : 'Anonymous'), 
+                                        content: text,
+                                           type: "postMessage" } ))
 
     // If no username, user is anonymous
 
